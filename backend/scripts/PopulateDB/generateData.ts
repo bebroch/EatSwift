9f3f01600891ee0ed4026f4d4038e2d67dce7b5a
kamasutra <97059293+bebroch@users.noreply.github.com>
2023-10-30 20:08:03 +0000

    update: populateDB

--- backend/scripts/PopulateDB/generateData.ts
+++ backend/scripts/PopulateDB/generateData.ts
@@ -1,77 +1,14 @@
-import { faker, fakerRU } from "@faker-js/faker";
-import { connectDB, disconnectDB } from "../../src/database/connect";
-import Restaurant from "../../src/models/Restaurant";
 import { ObjectId } from "mongoose";
-import Menu from "../../src/models/Menu";
+import { connectDB, disconnectDB } from "../../src/database/connect";
 import { IMenu } from "../../src/interface/Menu";
-import { IRestaurant } from "../../src/interface/Restaurant";
-import { IDish } from "../../src/interface/Dish";
-import { IUser } from "../../src/interface/User";
 import Dish from "../../src/models/Dish";
+import Menu from "../../src/models/Menu";
+import Restaurant from "../../src/models/Restaurant";
 import User from "../../src/models/User";
-
-function createRandomRestaurant() {
-	return {
-		name: fakerRU.company.name(),
-		email: faker.internet.email(),
-		description: faker.commerce.productDescription(),
-		address: faker.location.city(),
-		contactInfo: faker.commerce.department(),
-		rating: faker.number.int({ min: 0, max: 10 }),
-		password: faker.internet.password(),
-		verified: <any>faker.number.int({ min: 0, max: 1 }),
-	};
-}
-
-function createRandomMenu() {
-	return {
-		restaurant_id: "",
-		name: fakerRU.commerce.productName(),
-		description: fakerRU.commerce.productName(),
-	};
-}
-fakerRU.database.mongodbObjectId;
-function createRandomDish() {
-	const numberOfIngredients = faker.number.int({ min: 1, max: 5 }); // Генерируем случайное количество ингредиентов от 1 до 5
-
-	const ingredients = Array.from({ length: numberOfIngredients }, () =>
-		fakerRU.commerce.productMaterial()
-	);
-
-	return {
-		restaurant_id: "",
-		menu_id: "",
-		name: fakerRU.commerce.productName(),
-		description: fakerRU.commerce.productDescription(),
-		ingredients: ingredients,
-		picture: fakerRU.commerce.productName(),
-		price: faker.number.int({ min: 0, max: 5000 }),
-	};
-}
-
-function createRandomUser() {
-	return {
-		login: faker.person.firstName(),
-		email: faker.internet.email(),
-		address: faker.location.city(),
-		phoneNumber: faker.person.firstName(),
-		password: faker.internet.password(),
-		verified: true,
-		cart: [],
-	};
-}
-
-async function getRandomRestaurant() {
-	const count = await Restaurant.countDocuments(); // Получите количество ресторанов в базе данных
-	const randomIndex = Math.floor(Math.random() * count); // Генерируйте случайный индекс
-
-	const randomRestaurant = await Restaurant.aggregate([
-		{ $skip: randomIndex }, // Пропустите случайное количество ресторанов
-		{ $limit: 1 }, // Выберите только один ресторан
-	]);
-
-	return randomRestaurant[0]; // Верните случайный ресторан
-}
+import createRandomDish from "./createRandomDish";
+import createRandomMenu from "./createRandomMenu";
+import createRandomRestaurant from "./createRandomRestaurant";
+import createRandomUser from "./createRandomUser";
 
 async function generate() {
 	await connectDB();
@@ -103,7 +40,7 @@ async function generate() {
 	}
 	for (let i = 0; i < 10; i++) {
 		const userData = createRandomUser();
-		const user = await User.createUser(userData);
+		await User.createUser(userData);
 	}
 
 	await disconnectDB();
